f6c9e3ef904cd427157717951a458f8740db72ee
Alex223124 <sologoubalex@gmail.com>
2019-09-30 11:27:10 +0100

    binding pry removed

--- app/models/exchange_operation.rb
+++ app/models/exchange_operation.rb
@@ -5,9 +5,7 @@ class ExchangeOperation < ApplicationRecord
   before_save :set_result
 
   def set_result
-    binding.pry
     rate = ExchangeRate.where(source_currency: self.from_currency, target_currency: self.to_currency).first.try(:rate)
-    binding.pry
     self.result = self.amount * rate
   end
 
