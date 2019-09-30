4c04abcb326507e031a93b24a1eaeef658253d9c
Alex223124 <sologoubalex@gmail.com>
2019-09-30 03:11:26 +0100

    typo fixed

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -7,7 +7,7 @@ class ExchangeRatesController < ApplicationController
   def create
     @service = Services::ExchangeRates::Retrive.new
     @service.perform
-    redirect_to exchange_operations_path, notice: 'Exchange rate was successfully created.'
+    redirect_to exchange_operations_path, notice: 'Exchange rates was successfully created.'
   rescue => e
     redirect_to action: :new, notice: e.message
   end
