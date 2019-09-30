c3e3d6ae39cee87b36b9e12df3feb38c8968caf6
Alex223124 <sologoubalex@gmail.com>
2019-09-30 03:09:37 +0100

    one validation removed

--- app/models/exchange_operation.rb
+++ app/models/exchange_operation.rb
@@ -1,11 +1,13 @@
 class ExchangeOperation < ApplicationRecord
 
-  validates_presence_of :from_currency, :amount, :to_currency, :result
+  validates_presence_of :from_currency, :amount, :to_currency
 
   before_save :set_result
 
   def set_result
+    binding.pry
     rate = ExchangeRate.where(source_currency: self.from_currency, target_currency: self.to_currency).first.try(:rate)
+    binding.pry
     self.result = self.amount * rate
   end
 
