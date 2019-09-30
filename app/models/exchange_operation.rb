4d0d649b1c0104410a3ffd01c73949b5ceb4d5ee
Alex223124 <sologoubalex@gmail.com>
2019-09-30 03:01:06 +0100

    exchange operation model added

---
+++ app/models/exchange_operation.rb
@@ -0,0 +1,12 @@
+class ExchangeOperation < ApplicationRecord
+
+  validates_presence_of :from_currency, :amount, :to_currency, :result
+
+  before_save :set_result
+
+  def set_result
+    rate = ExchangeRate.where(source_currency: self.from_currency, target_currency: self.to_currency).first.try(:rate)
+    self.result = self.amount * rate
+  end
+
+end
